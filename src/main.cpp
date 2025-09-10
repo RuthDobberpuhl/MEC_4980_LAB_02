b94c5c10e93efaecb65eecad5f8754f51ced7eab
RuthDobberpuhl <dobberpuhlr@msoe.edu>
2025-09-10 12:37:23 -0500

    opposite lighter darker

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,22 @@
 int myFunction(int, int);
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(960);
   Serial.println("Starting Reads");
 }
 
 int ledBrightness = 0;
+int reading2 = 0;
 
 void loop() {
-  delay(10);
+  delay(250); // read every .25 seconds
   int reading = analogRead(A0);
   Serial.print("Result: ");
-  Serial.println(reading);
-  Serial.println("");
-  analogWrite(LED_BUILTIN, reading/8);
-  ledBrightness = (ledBrightness+ 1) % 256;
+  Serial.println(reading/16); 
+  reading2 = 256-reading/16;
+  analogWrite(LED_BUILTIN, reading2);
+  //ledBrightness = (ledBrightness+ 1) % 256;
+  Serial.println(reading2);
+  
 }
+
